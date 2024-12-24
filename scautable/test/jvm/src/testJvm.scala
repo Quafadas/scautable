e19755a63a0a21e8f1f1084b98b73ce9afdd9047
Simon Parten <quafadas@gmail.com>
2024-12-24 09:30:38 +0100

    .

--- scautable/test/jvm/src/testJvm.scala
+++ scautable/test/jvm/src/testJvm.scala
@@ -16,6 +16,21 @@ class JVMSuite extends munit.FunSuite {
     }
     case class Customize(t: LocalDate, i: Int)
     val custom = Seq(Customize(LocalDate.of(2025, 1, 1), 1))
-    assertEquals("""<table id="scautable" class="display"><thead><tr><th>t</th><th>i</th></tr></thead><tbody><tr><td>2025-01-01</td><td>1</td></tr></tbody></table>""", scautable(custom).toString())
+    assertEquals(
+      """<table id="scautable" class="display"><thead><tr><th>t</th><th>i</th></tr></thead><tbody><tr><td>2025-01-01</td><td>1</td></tr></tbody></table>""",
+      scautable(custom).toString()
+    )
+  }
+
+  test("console") {
+    case class ScauTest(anInt: Int, aString: String)
+    val start    = ScauTest(1, "2")
+    val start2   = ScauTest(2, "booyakashah")
+    val startSeq = Seq(start, start2)
+    val console  = scautable.consoleFormat(startSeq)
+    assertEquals(
+      console,
+      s"""| |anInt|    aString|\n+-+-----+-----------+\n|0|    1|          2|\n+-+-----+-----------+\n|1|    2|booyakashah|\n+-+-----+-----------+"""
+    )
   }
 }
