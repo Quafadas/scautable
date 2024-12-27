91827f1ee29af1fa4a72121db73a3841272767b9
partens <simon.parten@schroders.com>
2024-12-27 22:34:01 +0100

    gotcha...

--- scautable/src/csv.scala
+++ scautable/src/csv.scala
@@ -10,11 +10,31 @@ import scala.collection.immutable.Stream.Empty
 @experimental
 object CSV:
 
-  class FileLineIterator[N <: Tuple, V <: Tuple](filePath: String) extends Iterator[NamedTuple[N, V]] {
+  extension (s: Seq[Product]) def pt = scautable.consoleFormat(s)
+
+  given IteratorToExpr[K, V](using ToExpr[String], Type[K], Type[V]): ToExpr[FileLineIterator[K, V & Tuple]] with
+    def apply(opt: FileLineIterator[K, V & Tuple])(using Quotes): Expr[FileLineIterator[K, V & Tuple]] =
+      val str = Expr(opt.getFilePath)
+      '{
+        new FileLineIterator[K, V & Tuple]($str)
+      }
+  given IteratorToExpr2[K](using ToExpr[String], Type[K]): ToExpr[FileLineIterator2[K]] with
+    def apply(opt: FileLineIterator2[K])(using Quotes): Expr[FileLineIterator2[K]] =
+      val str = Expr(opt.getFilePath)
+      '{
+        new FileLineIterator2[K]($str)
+      }
+
+    // given IteratorStrings[Iterator[String]](using ToExpr[String]): ToExpr[Iterator[String]] with
+    //   def apply(opt: scala.collection.Iterator[String])(using Quotes): Expr[Iterator[String]] =
+    //     Expr(opt.toList)
+
+  class FileLineIterator[K, V <: Tuple](filePath: String) extends Iterator[NamedTuple[K & Tuple, V]] {
     def getFilePath: String  = filePath
     private val source       = Source.fromFile(filePath)
     private val lineIterator = source.getLines()
     private var lineNumber   = 0
+    private var isFirstLine  = true
 
     override def hasNext: Boolean = {
       val hasMore = lineIterator.hasNext
@@ -22,9 +42,47 @@ object CSV:
       hasMore
     }
 
-    override def next(): NamedTuple[N, V] = {
+    def listToTuple(list: List[String]): Tuple = list match {
+      case Nil    => EmptyTuple
+      case h :: t => h *: listToTuple(t)
+    }
+
+    override def next(): NamedTuple[K & Tuple, V] = {
       if (!hasNext) throw new NoSuchElementException("No more lines")
-      ???
+      val str = lineIterator.next()
+      isFirstLine = false
+      val splitted = str.split(",").toList
+      val tuple    = listToTuple(splitted).asInstanceOf[V]
+      NamedTuple.build[K & Tuple]()(tuple)
+      // (lineNumber, lineIterator.next()) // Return line number and line content
+    }
+  }
+
+  class FileLineIterator2[K](filePath: String) extends Iterator[NamedTuple[K & Tuple, K & Tuple]] {
+    def getFilePath: String  = filePath
+    private val source       = Source.fromFile(filePath)
+    private val lineIterator = source.getLines()
+    private var lineNumber   = 0
+    private var isFirstLine  = true
+
+    override def hasNext: Boolean = {
+      val hasMore = lineIterator.hasNext
+      if (!hasMore) source.close() // Close the file when done
+      hasMore
+    }
+
+    def listToTuple(list: List[String]): Tuple = list match {
+      case Nil    => EmptyTuple
+      case h :: t => h *: listToTuple(t)
+    }
+
+    override def next(): NamedTuple[K & Tuple, K & Tuple] = {
+      if (!hasNext) throw new NoSuchElementException("No more lines")
+      val str = lineIterator.next()
+      isFirstLine = false
+      val splitted = str.split(",").toList
+      val tuple    = listToTuple(splitted).asInstanceOf[K & Tuple]
+      NamedTuple.build[K & Tuple]()(tuple)
       // (lineNumber, lineIterator.next()) // Return line number and line content
     }
   }
@@ -33,11 +91,6 @@ object CSV:
 
   def readCsvAsNamedTupleTypeImpl2(pathExpr: Expr[String])(using Quotes) = {
     import quotes.reflect._
-
-    def listToTuple(list: List[String]): Tuple = list match {
-      case Nil    => EmptyTuple
-      case h :: t => h *: listToTuple(t)
-    }
 
     import quotes.reflect.*
 
@@ -52,13 +105,18 @@ object CSV:
     val tupleExpr2 = Expr.ofTupleFromSeq(headers.map(Expr(_)))
     tupleExpr2 match
       case '{ $tup: t } =>
-        given IteratorToExpr[K, V: Type: ToExpr]: ToExpr[FileLineIterator[K, V]] with
-          def apply(opt: FileLineIterator[K, V])(using Quotes): Expr[FileLineIterator[K, V]] =
-            val str = opt.getFilePath
-            '{ new FileLineIterator[K, V](${ Expr(str) }) }
-
-        val itr: FileLineIterator[t & Tuple, Tuple] = new FileLineIterator[t & Tuple, Tuple](path)
-
-        Expr(itr)
+        val itr: FileLineIterator[t, Tuple] = new FileLineIterator[t, Tuple](path)
+        '{ NamedTuple.build[t & Tuple]()($tup) }
+      // '{ itr }
+      // Expr(itr)
       case _ => report.throwError(s"Could not summon Type for type: ${tupleExpr2.show}")
+
+    tupleExpr2 match
+      case '{ $tup: t } =>
+        val itr = new FileLineIterator2[t](path)
+        // '{ NamedTuple.build[t & Tuple]()($tup) }
+        Expr(itr)
+      // Expr(itr)
+      case _ => report.throwError(s"Could not summon Type for type: ${tupleExpr2.show}")
+
   }
--- scautable/test/jvm/src/testJvm.scala
+++ scautable/test/jvm/src/testJvm.scala
@@ -4,7 +4,8 @@ import scalatags.Text.all.*
 import java.time.LocalDate
 
 import scala.annotation.experimental
-import NamedTuple.withNames
+import NamedTuple.*
+import CSV.*
 
 @experimental
 class JVMSuite extends munit.FunSuite {
@@ -28,20 +29,26 @@ class JVMSuite extends munit.FunSuite {
 
   test("csv") {
 
-    val tupleExample                                = (col1 = "a", col2 = "b")
+    val tupleExample: (col1: String, col2: String)  = (col1 = "a", col2 = "b")
     val tupleExample2: (col1: String, col2: String) = NamedTuple.build[("col1", "col2")]()(("a", "b"))
 
     val c = ("a", "b").withNames[("col1", "col2")]
 
-    // val tupl3 = NamedTuple
+    val itr = List(tupleExample2, tupleExample, c).iterator
+    println(itr.map(_.col1).toArray.mkString(","))
 
     println(tupleExample.col1)
     println(tupleExample2.col2)
     println(c.col1)
 
     val csv = CSV.readCsvAsNamedTupleType("C:\\temp\\scautable\\simple.csv")
-    println(csv)
-    println(csv.col1)
+    // println(csv.drop(1).toArray.mkString("\n"))
+    // csv.toList.map(x => x.col1)
+    val values = csv.drop(1).toArray
+    println(values.map(_.col1.toInt).mkString(","))
+
+    // println(Seq(csv).pt)
+    // println(csv.col1)
     // println(csv)
     // val a = NamedTuple.build[csv.type]()[Tuple2[String, String]]("a", "b")
     // val a = NamedTuple.build[csv.TYPE]()[Tuple2[String, String]]("a", "b")
