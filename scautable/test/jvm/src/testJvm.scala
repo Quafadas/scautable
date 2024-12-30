8d1e14a36c0facdff2b42c4a17de227773a91243
Simon Parten <quafadas@gmail.com>
2024-12-30 08:24:06 +0100

    .

--- scautable/test/jvm/src/testJvm.scala
+++ scautable/test/jvm/src/testJvm.scala
@@ -29,28 +29,15 @@ class JVMSuite extends munit.FunSuite:
 
   test("csv") {
 
-    // val tupleExample: (col1: String, col2: String, col3: String) = (col1 = "1", col2 = "2", col3 = "three")
-    // val tupleExample2: (col1: String, col2: String)              = NamedTuple.build[("col1", "col2")]()(("a", "b"))
+    // TODO: Docs
 
-    // val c = ("a", "b").withNames[("col1", "col2")]
+    // TODO: Sources : URL, pwd, resource
 
-    // val temp = tupleExample.col1.toInt
-    // println(temp)
+    // TODO: Sample (graduated)
 
-    // val itr = List(tupleExample2, tupleExample, c).iterator
-    // // println(itr.map(_.col1).toArray.mkString(","))
-
-    // println(tupleExample.col1)
-    // println(tupleExample2.col2)
-    // println(c.col1)
+    // TODO : Desktop show
 
     val csv: CsvIterator[("col1", "col2")] = CSV.readCsvAsNamedTupleType("/Users//simon//Code//scautable//simple.csv")
-
-    val newItr: CsvIterator[("col1", "col2")] = csv.copy()
-    // println(csv.drop(1).toArray.mkString("\n"))
-    // csv.toList.map(x => x.col1)
-    println(csv.headers)
-    println(csv.headerIndex("col2"))
 
     val col1 = csv.column(x => x.col1.toInt)
 
@@ -64,17 +51,13 @@ class JVMSuite extends munit.FunSuite:
           .withNames[csv.COLUMNS]
       }
       .toArray
-    println(values.mkString("\n"))
 
-    println(newItr.toArray.mkString("\n"))
+    println(values.consolePrint(csv.headers))
 
-    // println(values.map(_.col1.toInt).mkString(","))
+    values.consolePrint
 
-    // println(Seq(csv).pt)
-    // println(csv.col1)
-    // println(csv)
-    // val a = NamedTuple.build[csv.type]()[Tuple2[String, String]]("a", "b")
-    // val a = NamedTuple.build[csv.TYPE]()[Tuple2[String, String]]("a", "b")
-
+    val newItr: CsvIterator[("col1", "col2")] = csv.copy()
+    println(newItr.drop(1).toArray.consolePrint(csv.headers))
+    desktopShowNt(values)
   }
 end JVMSuite
