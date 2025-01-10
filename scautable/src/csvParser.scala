922eb1a47197f9ce940e3ca5531f5f3fd425ae93
Simon Parten <quafadas@gmail.com>
2025-01-10 12:18:51 +0100

    add an excel version

---
+++ scautable/src/csvParser.scala
@@ -0,0 +1,49 @@
+package io.github.quafadas.scautable
+
+import scala.io.Source
+import scala.util.Try
+import scala.util.chaining.*
+import scala.util.matching.Regex
+
+
+  /**
+    * According to chatGPT will parse RFC 4180 compliant CSV.
+    */
+  object CSVParser {
+    def parseLine(line: String, delimiter: Char = ',', quote: Char = '"'): List[String] = {
+      var inQuotes = false
+      val cellBuffer = new StringBuilder
+      val result = scala.collection.mutable.ListBuffer.empty[String]
+
+      for (char <- line) {
+        char match {
+          case `quote` if !inQuotes =>
+            // Start of quoted section
+            inQuotes = true
+
+          case `quote` if inQuotes =>
+            // End of quoted section (peek ahead to handle escaped quotes)
+            if (cellBuffer.nonEmpty && cellBuffer.last == quote) {
+              cellBuffer.deleteCharAt(cellBuffer.length - 1) // Handle escaped quote
+              cellBuffer.append(char)
+            } else {
+              inQuotes = false
+            }
+
+          case `delimiter` if !inQuotes =>
+            // Delimiter outside quotes ends the current cell
+            result.append(cellBuffer.toString)
+            cellBuffer.clear()
+
+          case _ =>
+            // Add character to the current cell
+            cellBuffer.append(char)
+        }
+      }
+
+      // Append the last cell, if any
+      result.append(cellBuffer.toString)
+
+      result.toList
+    }
+  }
--- scautable/src/package.scala
+++ scautable/src/package.scala
@@ -5,5 +5,8 @@ object table:
   export io.github.quafadas.scautable.CSV
   export io.github.quafadas.scautable.CSV.*
   export io.github.quafadas.scautable.scautable
+  export io.github.quafadas.scautable.CsvIterator
+  export io.github.quafadas.scautable.Excel
+  export io.github.quafadas.scautable.Excel.*
   export io.github.quafadas.scautable.scautable.*
   export io.github.quafadas.scautable.ConsoleFormat.*
