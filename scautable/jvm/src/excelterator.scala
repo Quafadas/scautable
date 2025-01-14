32ba69b1b11ea541b9d90577bfc4733b341342d1
Simon Parten <quafadas@gmail.com>
2025-01-14 09:01:41 +0100

    .

--- scautable/jvm/src/excelterator.scala
+++ scautable/jvm/src/excelterator.scala
@@ -8,14 +8,12 @@ import scala.NamedTuple.*
 import scala.compiletime.*
 import CSV.*
 import ConsoleFormat.*
-import org.apache.poi.ss.usermodel.{ DataFormatter, WorkbookFactory, Row }
+import org.apache.poi.ss.usermodel.{DataFormatter, WorkbookFactory, Row}
 import java.io.File
 import scala.collection.JavaConverters.*
 import scala.quoted.*
 
-
 object Excel:
-
 
   given IteratorToExpr2[K](using ToExpr[String], Type[K]): ToExpr[ExcelIterator[K]] with
     def apply(opt: ExcelIterator[K])(using Quotes): Expr[ExcelIterator[K]] =
@@ -27,7 +25,7 @@ object Excel:
     end apply
   end IteratorToExpr2
 
-  transparent inline def absolutePath[K](filePath: String, sheetName: String)= ${ readExcelAbolsutePath('filePath, 'sheetName) }
+  transparent inline def absolutePath[K](filePath: String, sheetName: String) = ${ readExcelAbolsutePath('filePath, 'sheetName) }
 
   def readExcelAbolsutePath(pathExpr: Expr[String], sheetName: Expr[String])(using Quotes) =
     import quotes.reflect.*
@@ -38,7 +36,6 @@ object Excel:
     tupleExpr2 match
       case '{ $tup: t } =>
 
-
         val itr = new ExcelIterator[t](fPath, sheetName.valueOrAbort)
         // println("tup")
         // println(tup)
@@ -46,29 +43,39 @@ object Excel:
         Expr(itr)
       case _ => report.throwError(s"Could not summon Type for type: ${tupleExpr2.show}")
     end match
+  end readExcelAbolsutePath
+end Excel
 
-
-class ExcelIterator[K](filePath: String, sheetName: String) extends Iterator[NamedTuple[K & Tuple, StringyTuple[K & Tuple] ]]:
+class ExcelIterator[K](filePath: String, sheetName: String) extends Iterator[NamedTuple[K & Tuple, StringyTuple[K & Tuple]]]:
   type COLUMNS = K
 
   def getFilePath: String = filePath
-  def getSheet : String = sheetName
-  lazy val sheetIterator = {
+  def getSheet: String = sheetName
+
+  lazy val sheetIterator =
     val workbook = WorkbookFactory.create(new File(filePath))
     val sheet = workbook.getSheet(sheetName)
     sheet.iterator().asScala
-  }
-  val headers = sheetIterator.next().cellIterator().asScala.toList.map(_.toString)
+  end sheetIterator
+
+  val headers = if sheetIterator.hasNext then sheetIterator.next().cellIterator().asScala.toList.map(_.toString) else List.empty
+
   lazy val headersTuple =
     listToTuple(headers)
 
+  var debugi: Int = 0
+  // var nextRow = sheetIterator.next().cellIterator().asScala.toList.map(_.toString)
   override def next(): NamedTuple[K & Tuple, StringyTuple[K & Tuple]] =
     if !hasNext then throw new NoSuchElementException("No more rows")
+    end if
     val row = sheetIterator.next()
     val cells = row.cellIterator().asScala.toList.map(_.toString)
+    if cells.size != headers.size then println(s"Row $debugi has ${cells.size} cells, but the table has ${headers.size} cells. Reading data will probably fail with an exception.")
+    end if
     val tuple = listToTuple(cells)
+    debugi += 1
     NamedTuple.build[K & Tuple]()(tuple).asInstanceOf[StringyTuple[K & Tuple]]
-
+  end next
 
   override def hasNext: Boolean = sheetIterator.hasNext
 
--- scautable/test/jvm/src/testExcel.scala
+++ scautable/test/jvm/src/testExcel.scala
@@ -1,6 +1,5 @@
 package io.github.quafadas.scautable
 
-
 import scalatags.Text.all.*
 import java.time.LocalDate
 
@@ -14,8 +13,19 @@ import ConsoleFormat.*
 class ExcelSuite extends munit.FunSuite:
 
   test("excel provider compiles and typechecks") {
-    val csv: ExcelIterator[("Column 1", "Column 2", "Column 3")] = Excel.absolutePath(Generated.resourceDir0 + "SimpleTable.xlsx", "Sheet1")
+    def csv: ExcelIterator[("Column 1", "Column 2", "Column 3")] = Excel.absolutePath(Generated.resourceDir0 + "SimpleTable.xlsx", "Sheet1")
 
     assertEquals(csv.column[("Column 1")].toList.head, "Row 1, Col 1")
+    assertEquals(csv.column[("Column 1")].toList.last, "Row 3, Col 1")
 
   }
+
+  test("excel provider compiles and typechecks with a blank row") {
+    val csv: ExcelIterator[("Column 1", "Column 2", "Column 3")] = Excel.absolutePath(Generated.resourceDir0 + "SimpleTableWithExtendedRow.xlsx", "Sheet1")
+
+    intercept[Exception] {
+      println(csv.toList)
+    }
+
+  }
+end ExcelSuite
