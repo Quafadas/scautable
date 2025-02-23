b44b63742b9689d7001f33f4f69f1c860207e660
Scala Steward <scala_steward@virtuslab.com>
2025-02-23 18:44:34 +0000

    Reformat with scalafmt 3.8.6
    Executed command: scalafmt --non-interactive

--- scautable/src/csvParser.scala
+++ scautable/src/csvParser.scala
@@ -5,45 +5,40 @@ import scala.util.Try
 import scala.util.chaining.*
 import scala.util.matching.Regex
 
+/** According to chatGPT will parse RFC 4180 compliant CSV.
+  */
+object CSVParser:
+  def parseLine(line: String, delimiter: Char = ',', quote: Char = '"'): List[String] =
+    var inQuotes = false
+    val cellBuffer = new StringBuilder
+    val result = scala.collection.mutable.ListBuffer.empty[String]
 
-  /**
-    * According to chatGPT will parse RFC 4180 compliant CSV.
-    */
-  object CSVParser {
-    def parseLine(line: String, delimiter: Char = ',', quote: Char = '"'): List[String] = {
-      var inQuotes = false
-      val cellBuffer = new StringBuilder
-      val result = scala.collection.mutable.ListBuffer.empty[String]
+    for char <- line do
+      char match
+        case `quote` if !inQuotes =>
+          // Start of quoted section
+          inQuotes = true
 
-      for (char <- line) {
-        char match {
-          case `quote` if !inQuotes =>
-            // Start of quoted section
-            inQuotes = true
-
-          case `quote` if inQuotes =>
-            // End of quoted section (peek ahead to handle escaped quotes)
-            if (cellBuffer.nonEmpty && cellBuffer.last == quote) {
-              cellBuffer.deleteCharAt(cellBuffer.length - 1) // Handle escaped quote
-              cellBuffer.append(char)
-            } else {
-              inQuotes = false
-            }
-
-          case `delimiter` if !inQuotes =>
-            // Delimiter outside quotes ends the current cell
-            result.append(cellBuffer.toString)
-            cellBuffer.clear()
-
-          case _ =>
-            // Add character to the current cell
+        case `quote` if inQuotes =>
+          // End of quoted section (peek ahead to handle escaped quotes)
+          if cellBuffer.nonEmpty && cellBuffer.last == quote then
+            cellBuffer.deleteCharAt(cellBuffer.length - 1) // Handle escaped quote
             cellBuffer.append(char)
-        }
-      }
+          else inQuotes = false
 
-      // Append the last cell, if any
-      result.append(cellBuffer.toString)
+        case `delimiter` if !inQuotes =>
+          // Delimiter outside quotes ends the current cell
+          result.append(cellBuffer.toString)
+          cellBuffer.clear()
 
-      result.toList
-    }
-  }
+        case _ =>
+          // Add character to the current cell
+          cellBuffer.append(char)
+    end for
+
+    // Append the last cell, if any
+    result.append(cellBuffer.toString)
+
+    result.toList
+  end parseLine
+end CSVParser
--- scautable/src/stringConversion.scala
+++ scautable/src/stringConversion.scala
@@ -2,17 +2,20 @@ package io.github.quafadas.scautable
 
 case class ConversionAcc(validInts: Long, validDoubles: Long, validLongs: Long)
 
-def recommendConversion(acc: List[ConversionAcc], rowCount: Long): String = {
+def recommendConversion(acc: List[ConversionAcc], rowCount: Long): String =
   def percentage(validCount: Long): Double = (validCount.toDouble / rowCount) * 100
 
-  acc.map { case ConversionAcc(validInts, validDoubles, validLongs) =>
-    val intPct = percentage(validInts)
-    val longPct = percentage(validLongs)
-    val doublePct = percentage(validDoubles)
+  acc
+    .map { case ConversionAcc(validInts, validDoubles, validLongs) =>
+      val intPct = percentage(validInts)
+      val longPct = percentage(validLongs)
+      val doublePct = percentage(validDoubles)
 
-    if (intPct >= 75.0 && intPct >= longPct && intPct >= doublePct) "Int"
-    else if (longPct >= 75.0 && longPct >= doublePct) "Long"
-    else if (doublePct >= 75.0) "Double"
-    else "String"
-  }.mkString(", ")
-}
+      if intPct >= 75.0 && intPct >= longPct && intPct >= doublePct then "Int"
+      else if longPct >= 75.0 && longPct >= doublePct then "Long"
+      else if doublePct >= 75.0 then "Double"
+      else "String"
+      end if
+    }
+    .mkString(", ")
+end recommendConversion
