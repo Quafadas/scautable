588ce6dcc5b4c584025a093dcf1e4bb73e09fb96
Simon Parten <quafadas@gmail.com>
2025-05-20 21:25:06 +0200

    scatter and regression

--- examples/src/titanic.scala
+++ examples/src/titanic.scala
@@ -96,13 +96,15 @@ end Gender
 
   group.ptbln
 
-  println("plots")
-  data.plotPieChart["Sex"]
-  data.plotPieChart["Survived"]
-  data.plotHistogram["Fare"]
-  data.plotPieChart["AgeIsDefined"]
-  data.filter(_.Age.isDefined).mapColumn["Age", Double](_.get).plotHistogram["Age"]
-  data.filter(_.Age.isDefined).mapColumn["Age", Double](_.get).plotMarginalHistogram["Age", "Fare"]
+  // println("plots")
+  // data.plotPieChart["Sex"]
+  // data.plotPieChart["Survived"]
+  // data.plotHistogram["Fare"]
+  // data.plotPieChart["AgeIsDefined"]
+  // data.filter(_.Age.isDefined).mapColumn["Age", Double](_.get).plotHistogram["Age"]
+  // data.filter(_.Age.isDefined).mapColumn["Age", Double](_.get).plotMarginalHistogram["Age", "Fare"]
+  data.filter(_.Age.isDefined).mapColumn["Age", Double](_.get).plotScatter["Age", "Fare"]
+  data.filter(_.Age.isDefined).mapColumn["Age", Double](_.get).plotRegression["Age", "Fare"]
 
 end titanic
 
@@ -215,5 +217,105 @@ extension [K <: Tuple, V <: Tuple](data: Seq[NamedTuple[K, V]])
       )
     )
   end plotMarginalHistogram
+
+  inline def plotScatter[S1 <: String, S2 <: String](using
+      @implicitNotFound("Column ${S1} not found")
+      ev1: IsColumn[S1, K] =:= true,
+      @implicitNotFound("Column ${S2} not found")
+      ev2: IsColumn[S2, K] =:= true,
+      ev: AllAreColumns[(S1, S2), K] =:= true,
+      s1: ValueOf[S1],
+      s2: ValueOf[S2],
+      @implicitNotFound("Column ${S1} is not numeric")
+      numeric1: Numeric[GetTypeAtName[K, S1, V]],
+      @implicitNotFound("Column ${S2} is not numeric")
+      numeric2: Numeric[GetTypeAtName[K, S2, V]]
+  ): Unit =
+    val column1 = data.column[S1]
+    val column2 = data.column[S2]
+    val zipped = column1.zip(column2)
+    val spec = os.resource / "scatter.vg.json"
+    val col1Name: String = s1.value
+    val col2Name: String = s2.value
+    spec.plot(
+      List(
+        spec =>
+          spec("data") = upickle.default.writeJs(
+            (values = zipped.map: d =>
+              ujson.Obj(
+                col1Name -> numeric1.toDouble(d._1),
+                col2Name -> numeric2.toDouble(d._2)
+              ))
+          ),
+        spec => spec("description") = s"${col1Name}-VS-${col2Name}",
+        spec => spec("encoding")("x")("field") = col1Name,
+        spec => spec("encoding")("y")("field") = col2Name,
+        spec =>
+          spec("title") = upickle.default.writeJs(
+            text = s"$col1Name vs $col2Name",
+            fontSize = (
+              expr = "width / 20"
+            )
+          )
+      )
+    )
+  end plotScatter
+
+  inline def plotRegression[S1 <: String, S2 <: String](using
+      @implicitNotFound("Column ${S1} not found")
+      ev1: IsColumn[S1, K] =:= true,
+      @implicitNotFound("Column ${S2} not found")
+      ev2: IsColumn[S2, K] =:= true,
+      ev: AllAreColumns[(S1, S2), K] =:= true,
+      s1: ValueOf[S1],
+      s2: ValueOf[S2],
+      @implicitNotFound("Column ${S1} is not numeric")
+      numeric1: Numeric[GetTypeAtName[K, S1, V]],
+      @implicitNotFound("Column ${S2} is not numeric")
+      numeric2: Numeric[GetTypeAtName[K, S2, V]]
+  ): Unit =
+    val column1 = data.column[S1]
+    val column2 = data.column[S2]
+    val zipped = column1.zip(column2)
+    val spec = os.resource / "regression.vg.json"
+    val col1Name: String = s1.value
+    val col2Name: String = s2.value
+    val encoding = (
+      x = (
+        field = col1Name,
+        `type` = "quantitative"
+      ),
+      y = (
+        field = col2Name,
+        `type` = "quantitative"
+      )
+    )
+    spec.plot(
+      List(
+        spec =>
+          spec("data") = upickle.default.writeJs(
+            (values = zipped.map: d =>
+              ujson.Obj(
+                col1Name -> numeric1.toDouble(d._1),
+                col2Name -> numeric2.toDouble(d._2)
+              ))
+          ),
+        spec => spec("description") = s"${col1Name}-VS-${col2Name}",
+        spec => spec("layer")(0)("encoding") = upickle.default.writeJs(encoding),
+        spec => spec("layer")(1)("encoding") = upickle.default.writeJs(encoding),
+        spec => spec("layer")(1)("transform")(0)("regression") = col2Name,
+        spec => spec("layer")(1)("transform")(0)("on") = col1Name,
+        spec => spec("layer")(2)("transform")(0)("regression") = col2Name,
+        spec => spec("layer")(2)("transform")(0)("on") = col1Name,
+        spec =>
+          spec("title") = upickle.default.writeJs(
+            text = s"$col1Name vs $col2Name",
+            fontSize = (
+              expr = "width / 20"
+            )
+          )
+      )
+    )
+  end plotRegression
 
 end extension
