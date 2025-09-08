dc2c4550f4b6c21ee29cfad7fb790b05491418c2
Simon Parten <Quafadas@users.noreply.github.com>
2025-09-08 07:51:09 +0000

    remove old type test

--- scautable/test/src/io/github/quafadas/scautable/CsvFromStringSuite.scala
+++ scautable/test/src/io/github/quafadas/scautable/CsvFromStringSuite.scala
@@ -73,14 +73,9 @@ class CsvFromStringSuite extends FunSuite:
   test("fromString should handle duplicated headers by parsing correctly but warn at compile time") {
     inline val csvContent = "colA,col2,col3,col4,col5,col6,col7,col8,col9,col10,col11,col12,col13,col14,col15,col116,col17,col18,col19,col20,col21,col22,col23,col24,col25,colA,col2,col3,col4,col5,col6,col7,col8,col9,col10\n1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,1,2"
     val csvIterator = CSV.fromString(csvContent, HeaderOptions.FromRows(1), TypeInferrer.FromTuple[(Int, String, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int,Int, Int, Int, Int, Int,Int, Int, Int, Int, Int)]())
-
-
-
     assert(csvIterator.hasNext)
     val row = csvIterator.next()
     assertEquals(row.colA, 1) 
-
-
     assert(!csvIterator.hasNext)
   }
 
