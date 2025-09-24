e3980a9b293e817eccd3d524ad66327d7a6ae6ef
Simon Parten <Quafadas@users.noreply.github.com>
2025-09-24 08:05:30 +0000

    Starting type Inf

--- scautable/src-jvm/excelterator.scala
+++ scautable/src-jvm/excelterator.scala
@@ -12,6 +12,26 @@ import org.apache.poi.ss.util.CellRangeAddress
 import io.github.quafadas.scautable.ColumnTyped.*
 import io.github.quafadas.table.TypeInferrer
 
+// Excel-specific decoders that can handle numeric strings like "1.0"
+// These need to be in scope when RowDecoder.summonAllDecoders is called
+inline given excelIntDecoder: Decoder[Int] with
+  def decode(str: String): Option[Int] =
+    str.toDoubleOption.flatMap { d =>
+      if d.isWhole && d >= Int.MinValue && d <= Int.MaxValue then
+        Some(d.toInt)
+      else
+        None
+    }.orElse(str.toIntOption) // fallback to regular int parsing
+
+inline given excelLongDecoder: Decoder[Long] with  
+  def decode(str: String): Option[Long] =
+    str.toDoubleOption.flatMap { d =>
+      if d.isWhole && d >= Long.MinValue && d <= Long.MaxValue then
+        Some(d.toLong)
+      else
+        None
+    }.orElse(str.toLongOption) // fallback to regular long parsing
+
 /** */
 object Excel:
 
@@ -28,8 +48,8 @@ object Excel:
     end apply
   end IteratorToExpr2
 
-  transparent inline def absolutePath[K](filePath: String, sheetName: String, range: String = "", typeInferrer: TypeInferrer = TypeInferrer.StringType) = ${ readExcelAbolsutePath('filePath, 'sheetName, 'range, 'typeInferrer) }
-  transparent inline def resource[K](filePath: String, sheetName: String, range: String = "", typeInferrer: TypeInferrer = TypeInferrer.StringType) = ${ readExcelResource('filePath, 'sheetName, 'range, 'typeInferrer) }
+  transparent inline def absolutePath[K](filePath: String, sheetName: String, range: String = "", inline typeInferrer: TypeInferrer = TypeInferrer.StringType) = ${ readExcelAbolsutePath('filePath, 'sheetName, 'range, 'typeInferrer) }
+  transparent inline def resource[K](filePath: String, sheetName: String, range: String = "", inline typeInferrer: TypeInferrer = TypeInferrer.StringType) = ${ readExcelResource('filePath, 'sheetName, 'range, 'typeInferrer) }
 
   def readExcelResource(pathExpr: Expr[String], sheetName: Expr[String], colRangeExpr: Expr[String], typeInferrerExpr: Expr[TypeInferrer])(using Quotes) =
     import quotes.reflect.*
@@ -40,15 +60,6 @@ object Excel:
     end if
     val validatedPath = resourcePath.toURI.getPath
     val colRange = colRangeExpr.value
-    
-    // Validate TypeInferrer at compile time
-    typeInferrerExpr match
-      case '{ TypeInferrer.StringType } => // OK, continue
-      case '{ TypeInferrer.FirstRow } => report.throwError("TypeInferrer FirstRow is not yet supported for Excel. Only StringType is currently supported.")
-      case '{ TypeInferrer.FromAllRows } => report.throwError("TypeInferrer FromAllRows is not yet supported for Excel. Only StringType is currently supported.")
-      case '{ TypeInferrer.FirstN(${ Expr(n) }) } => report.throwError(s"TypeInferrer FirstN($n) is not yet supported for Excel. Only StringType is currently supported.")
-      case '{ TypeInferrer.FromTuple[t]() } => report.throwError("TypeInferrer FromTuple is not yet supported for Excel. Only StringType is currently supported.")
-      case _ => report.throwError("Only TypeInferrer.StringType is currently supported for Excel")
     
     // Create a simple ExcelIterator just to get the headers at compile time
     val tempFile = new java.io.File(validatedPath)
@@ -82,9 +93,26 @@ object Excel:
         new ExcelIterator[Hdrs, StringyTuple[Hdrs]](${ Expr(validatedPath) }, $sheetName, ${ Expr(colRange) })
       }
     
+    def constructWithTypes[Hdrs <: Tuple : Type, V <: Tuple : Type]: Expr[ExcelIterator[Hdrs, V]] =
+      '{
+        new ExcelIterator[Hdrs, V](${ Expr(validatedPath) }, $sheetName, ${ Expr(colRange) })
+      }
+    
     tupleExpr2 match
-      case '{ $tup: t } =>
-        constructWithStringTypes[t & Tuple]
+      case '{ $tup: hdrs } =>
+        typeInferrerExpr match
+          case '{ TypeInferrer.FromTuple[t]() } =>
+            constructWithTypes[hdrs & Tuple, t & Tuple]
+          case '{ TypeInferrer.StringType } =>
+            constructWithStringTypes[hdrs & Tuple]
+          case '{ TypeInferrer.FirstRow } => 
+            report.throwError("TypeInferrer.FirstRow is not yet supported for Excel. Only StringType and FromTuple are currently supported.")
+          case '{ TypeInferrer.FromAllRows } => 
+            report.throwError("TypeInferrer.FromAllRows is not yet supported for Excel. Only StringType and FromTuple are currently supported.")
+          case '{ TypeInferrer.FirstN(${ Expr(n) }) } => 
+            report.throwError(s"TypeInferrer.FirstN($n) is not yet supported for Excel. Only StringType and FromTuple are currently supported.")
+          case _ => 
+            report.throwError("Only TypeInferrer.StringType and TypeInferrer.FromTuple are currently supported for Excel")
       case _ => report.throwError(s"Could not summon Type for type: ${tupleExpr2.show}")
     end match
   end readExcelResource
@@ -94,15 +122,6 @@ object Excel:
 
     val fPath = pathExpr.valueOrAbort
     val colRange = colRangeExpr.value
-    
-    // Validate TypeInferrer at compile time
-    typeInferrerExpr match
-      case '{ TypeInferrer.StringType } => // OK, continue
-      case '{ TypeInferrer.FirstRow } => report.throwError("TypeInferrer FirstRow is not yet supported for Excel. Only StringType is currently supported.")
-      case '{ TypeInferrer.FromAllRows } => report.throwError("TypeInferrer FromAllRows is not yet supported for Excel. Only StringType is currently supported.")
-      case '{ TypeInferrer.FirstN(${ Expr(n) }) } => report.throwError(s"TypeInferrer FirstN($n) is not yet supported for Excel. Only StringType is currently supported.")
-      case '{ TypeInferrer.FromTuple[t]() } => report.throwError("TypeInferrer FromTuple is not yet supported for Excel. Only StringType is currently supported.")
-      case _ => report.throwError("Only TypeInferrer.StringType is currently supported for Excel")
     
     // Create a simple ExcelIterator just to get the headers at compile time
     val tempFile = new java.io.File(fPath)
@@ -136,9 +155,26 @@ object Excel:
         new ExcelIterator[Hdrs, StringyTuple[Hdrs]](${ Expr(fPath) }, $sheetName, ${ Expr(colRange) })
       }
 
+    def constructWithTypes[Hdrs <: Tuple : Type, V <: Tuple : Type]: Expr[ExcelIterator[Hdrs, V]] =
+      '{
+        new ExcelIterator[Hdrs, V](${ Expr(fPath) }, $sheetName, ${ Expr(colRange) })
+      }
+
     tupleExpr2 match
-      case '{ $tup: t } =>
-        constructWithStringTypes[t & Tuple]
+      case '{ $tup: hdrs } =>
+        typeInferrerExpr match
+          case '{ TypeInferrer.FromTuple[t]() } =>
+            constructWithTypes[hdrs & Tuple, t & Tuple]
+          case '{ TypeInferrer.StringType } =>
+            constructWithStringTypes[hdrs & Tuple]
+          case '{ TypeInferrer.FirstRow } => 
+            report.throwError("TypeInferrer.FirstRow is not yet supported for Excel. Only StringType and FromTuple are currently supported.")
+          case '{ TypeInferrer.FromAllRows } => 
+            report.throwError("TypeInferrer.FromAllRows is not yet supported for Excel. Only StringType and FromTuple are currently supported.")
+          case '{ TypeInferrer.FirstN(${ Expr(n) }) } => 
+            report.throwError(s"TypeInferrer.FirstN($n) is not yet supported for Excel. Only StringType and FromTuple are currently supported.")
+          case _ => 
+            report.throwError("Only TypeInferrer.StringType and TypeInferrer.FromTuple are currently supported for Excel")
       case _ => report.throwError(s"Could not summon Type for type: ${tupleExpr2.show}")
     end match
   end readExcelAbolsutePath
--- scautable/test/src-jvm/testExcel.scala
+++ scautable/test/src-jvm/testExcel.scala
@@ -69,24 +69,49 @@ class ExcelSuite extends munit.FunSuite:
 
   }
 
+  test("excel provider with FromTuple TypeInferrer enforces specific types") {
+    // Force specific types using FromTuple - all columns are actually strings in SimpleTable.xlsx
+    def csv = Excel.resource("SimpleTable.xlsx", "Sheet1", "", TypeInferrer.FromTuple[(String, String, String)]())
+    
+    // Test that we can access the data with correct types
+    assertEquals(csv.column["Column 1"].toList.head, "Row 1, Col 1")  // String column
+    assertEquals(csv.column["Column 2"].toList.head, "Row 1, Col 2")  // String column  
+    assertEquals(csv.column["Column 3"].toList.head, "Row 1, Col 3")  // String column
+    
+    // Verify we can access the data
+    assertEquals(csv.size, 3) // 3 data rows
+  }
+
+  test("excel provider with FromTuple TypeInferrer (Double, Int, Long String)") {    
+    def csv = Excel.resource("Numbers.xlsx", "Sheet1", "", TypeInferrer.FromTuple[(Double, Int, Long, String)]())
+    
+    // Test that we can access the data with correct types
+    assertEquals(csv.column["Doubles"].toList.head, 1.1)
+    assertEquals(csv.column["Int"].toList.head, 1) 
+    assertEquals(csv.column["Longs"].toList.head, 1L)
+    
+    // Verify we can access the data
+    assertEquals(csv.size, 2) // 3 data rows
+  }
+
   test("excel provider rejects unsupported TypeInferrer at compile time") {
     assert(
       compileErrors(
         """Excel.resource("SimpleTable.xlsx", "Sheet1", "", TypeInferrer.FirstRow)"""
-      ).contains("TypeInferrer FirstRow is not yet supported for Excel")
+      ).contains("TypeInferrer.FirstRow is not yet supported for Excel. Only StringType and FromTuple are currently supported.")
     )
 
     assert(
       compileErrors(
         """Excel.resource("SimpleTable.xlsx", "Sheet1", "", TypeInferrer.FromAllRows)"""
-      ).contains("TypeInferrer FromAllRows is not yet supported for Excel")
+      ).contains("TypeInferrer.FromAllRows is not yet supported for Excel. Only StringType and FromTuple are currently supported.")
     )
     
     // FirstN falls back to the general case
     val firstNError = compileErrors(
         """Excel.resource("SimpleTable.xlsx", "Sheet1", "", TypeInferrer.FirstN(5))"""
     )
-    assert(firstNError.contains("Only TypeInferrer.StringType is currently supported for Excel"))
+    assert(firstNError.contains("TypeInferrer.FirstN(5) is not yet supported for Excel. Only StringType and FromTuple are currently supported."))
   }
 
 end ExcelSuite
