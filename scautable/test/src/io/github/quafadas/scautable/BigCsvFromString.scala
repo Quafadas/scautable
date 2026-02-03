58c329887de02175b9a50477d3b28f299825331a
partens <simon.parten@schroders.com>
2026-02-03 17:57:23 +0100

    .

--- scautable/test/src/io/github/quafadas/scautable/BigCsvFromString.scala
+++ scautable/test/src/io/github/quafadas/scautable/BigCsvFromString.scala
@@ -10,11 +10,9 @@ import munit.FunSuite
 
 class CsvFromBigStringSuite extends FunSuite:
 
-  test("fromString should handle duplicated headers by parsing correctly but warn at compile time") {
-    inline val csvContent: String =
-      "colA,col2,col3,col4,col5,col6,col7,col8,col9,col10,col11,col12,col13,col14,col15,col116,col17,col18,col19,col20,col21,col22,col23,col24,col25,colA,col2,col3,col4,col5,col6,col7,col8,col9,col10\n1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3"
+  test("fromString should handle duplicated headers by parsing correctly but warn at compile time") {    
     val csvIterator = CSV.fromString(
-      csvContent,
+      "colA,col2,col3,col4,col5,col6,col7,col8,col9,col10,col11,col12,col13,col14,col15,col116,col17,col18,col19,col20,col21,col22,col23,col24,col25,colA,col2,col3,col4,col5,col6,col7,col8,col9,col10\n1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3",
       HeaderOptions.FromRows(1),
       TypeInferrer.FromTuple[
         (
