c2b44afeb98485a5a93d70b56bbec0cb9491a761
Simon Parten <Quafadas@users.noreply.github.com>
2026-07-21 14:36:00 +0000

    .

--- scalasql/test/jvm/src/postgres.scala
+++ scalasql/test/jvm/src/postgres.scala
@@ -14,15 +14,25 @@ class PgSpec extends FunSuite with TestContainerForAll:
   override val containerDef = PostgreSQLContainer.Def(
     dockerImageName = DockerImageName.parse("postgres:15.1"),
     databaseName = "testcontainer-scala",
-    username = "scala",
-    password = "scala"
+    username = sys.env("SCAUTABLE_DB_USER"),
+    password = sys.env("SCAUTABLE_DB_PASSWORD")
   )
 
   test("PostgreSQL container should get connection") {
     withContainers { case pgContainer: PostgreSQLContainer =>
       Class.forName(pgContainer.driverClassName)
-      val connection = DriverManager.getConnection(pgContainer.jdbcUrl, pgContainer.username, pgContainer.password)
+      val connection = DriverManager.getConnection(pgContainer.jdbcUrl, sys.env("SCAUTABLE_DB_USER"), sys.env("SCAUTABLE_DB_PASSWORD"))
+      println(pgContainer.jdbcUrl)
       assert(!connection.isClosed())
+
+      val statement = connection.createStatement()
+      val createTable = "CREATE TABLE test_table (id SERIAL PRIMARY KEY, name VARCHAR(50), town text, something double precision);"
+      statement.execute(createTable)
+
+
+
+
+
     }
   }
 end PgSpec
